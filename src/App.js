e407cce802b14536c1943e61fd919da43a3d2f85
toplenboren <toplenboren@yandex.ru>
2021-03-22 19:48:01 +0500

    refactoring: remove obsolete code

--- src/App.js
+++ src/App.js
@@ -3,16 +3,15 @@ import PokemonListTemplate from "./components/PokemonListTemplate";
 import PokemonListPaginationTemplate from "./components/PokemonListPaginationTemplate";
 import {useState} from "react";
 import PokemonListLoaderTemplate from "./components/PokemonListLoaderTemplate";
+import {MAX_POKEMONS_LIMIT, POKEMON_API_BASE_URL} from "./config";
 
 function App() {
+    function getPokemonListByPageURL(page) {
+        return `${POKEMON_API_BASE_URL}/pokemon?limit=${MAX_POKEMONS_LIMIT}&offset=${page * MAX_POKEMONS_LIMIT}`
+    }
 
-    const MAX_POKEMONS_LIMIT = 12
-    const POKEMON_API_BASE_URL = 'https://pokeapi.co/api/v2'
-
-    const getPokemonListURL = (limit, offset) => (`${POKEMON_API_BASE_URL}/pokemon?limit=${limit}&offset=${offset}`)
-
-    async function getPokemonList(limit, offset) {
-        const pokemonList = await fetch(getPokemonListURL(limit, offset)).then(r => r.json())
+    async function getPokemonListByPage(page) {
+        const pokemonList = await fetch(getPokemonListByPageURL(page)).then(r => r.json())
         const richPokemonResults = await Promise.all(pokemonList.results.map(pokemonObject => fetch(pokemonObject.url).then(r => r.json())))
 
         const calculatedPageCount = Math.ceil(pokemonList.count / MAX_POKEMONS_LIMIT)
@@ -27,30 +26,19 @@ function App() {
     const [currentPage, setCurrentPage] = useState(1)
     const [totalPages, setTotalPages] = useState(null)
 
-    const {data, error} = useSWR(getPokemonListURL(MAX_POKEMONS_LIMIT, _calculateOffsetByPage()), () => getPokemonList(MAX_POKEMONS_LIMIT, _calculateOffsetByPage()))
+    const {data, error} = useSWR(getPokemonListByPageURL(currentPage), () => getPokemonListByPage(currentPage))
 
-    function _handlePageChange(page) {
+    function _handlePageChange(e, page) {
         setCurrentPage(page)
-    }
-
-    function _calculateOffsetByPage() {
-        return currentPage * MAX_POKEMONS_LIMIT
-    }
-
-    function _resolvePokemonList() {
-        if (data) {
-            return <PokemonListTemplate data={data.results}/>
-        }
-        return <PokemonListLoaderTemplate/>
     }
 
     if (error) return <div>failed to load</div>
     return (
         <>
-            {_resolvePokemonList()}
+            {data ? <PokemonListTemplate data={data.results}/> : <PokemonListLoaderTemplate/>}
             <PokemonListPaginationTemplate count={totalPages}
                                            page={currentPage}
-                                           onChange={(e, page) => _handlePageChange(page)}/>
+                                           onChange={_handlePageChange}/>
         </>
     );
 }
---
+++ src/config.js
@@ -0,0 +1,2 @@
+export const MAX_POKEMONS_LIMIT = 12
+export const POKEMON_API_BASE_URL = 'https://pokeapi.co/api/v2'
